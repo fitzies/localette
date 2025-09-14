e3cb89bb273760e38e00f91a0f20a530e434d36e
fitzies <olicrypt@gmail.com>
2025-09-14 17:03:50 +0800

    grid for page

--- src/app/admin/[shop]/page.tsx
+++ src/app/admin/[shop]/page.tsx
@@ -1,3 +1,9 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { getBusiness } from "@/lib/actions";
 
 export default async function Page({
@@ -10,9 +16,26 @@ export default async function Page({
   const business = await getBusiness(shop);
 
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
-      {JSON.stringify(business)}
-    </div>
+    <main className="grid grid-cols-3 gap-4 ">
+      <Card>
+        <CardHeader>
+          <CardDescription>Sales</CardDescription>
+          <CardTitle>$0.00</CardTitle>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardDescription>Orders</CardDescription>
+          <CardTitle>0</CardTitle>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardDescription>Views</CardDescription>
+          <CardTitle>0</CardTitle>
+        </CardHeader>
+      </Card>
+      <Card className="col-span-3"></Card>
+    </main>
   );
 }
